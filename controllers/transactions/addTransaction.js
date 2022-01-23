12711b1ee6095f4025ce6c4351585b7975080ba6
Kateryna Karpova <katyakarpova@gmail.com>
2022-01-23 13:38:09 +0100

    Update addTransaction.js
    Добавила логику пересчета баланса

--- controllers/transactions/addTransaction.js
+++ controllers/transactions/addTransaction.js
@@ -1,9 +1,10 @@
-const { NotFound } = require('http-errors');
+const { NotFound, BadRequest } = require('http-errors');
 const { Transaction } = require('../../models');
 const { Category } = require('../../models');
+const { User } = require('../../models');
 
 const addTransaction = async (req, res) => {
-  const { _id } = req.user;
+  const { _id, balance } = req.user;
   
   const { created_at, subcategory, category, transactionType, costs, incomes, year, month, day } =
   req.body;
@@ -33,12 +34,24 @@ const addTransaction = async (req, res) => {
     owner: _id,
   };
 
+  const updateBalance = incomes === false ? balance - costs : balance + costs;
+
+  if (updateBalance < 0) {
+    throw new BadRequest('There are no enough money for this purchase');
+  }
+
+  await User.findByIdAndUpdate(
+    { _id },
+    { balance: updateBalance }
+  );
+
   const result = await Transaction.create(newTransaction);
   res.status(201).json({
     status: 'succes',
     code: 201,
     data: {
       result,
+      balance: updateBalance,
     },
   });
 };
