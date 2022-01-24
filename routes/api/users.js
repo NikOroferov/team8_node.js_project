4832f5d606dab39a8830a51f6c0445a1f98d048e
Nikolay Yaryhin <77698141+NikOroferov@users.noreply.github.com>
2022-01-24 16:48:04 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,6 +18,6 @@ router.get('/verify/:verificationToken', ctrlWrapper(ctrl.verify));
 
 router.post('/verify', ctrlWrapper(ctrl.repeatVerifyEmail));
 
-router.get('/balance', auth, ctrlWrapper(ctrl.balance));
+router.patch('/balance', auth, ctrlWrapper(ctrl.balance));
 
 module.exports = router;
