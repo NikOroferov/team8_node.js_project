91d1b011b2a0c47d3a0e8276327d9a60cf520d4c
VasylBarna <vasylbarna@gmail.com>
2022-01-25 18:09:55 +0200

    fix register

--- models/user.js
+++ models/user.js
@@ -45,11 +45,9 @@ const userSchema = Schema(
   { versionKey: false, timestamps: true },
 );
 
-userSchema.pre('save', async function () {
-  if (this.isNew || this.isModified) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-});
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
 
 userSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
