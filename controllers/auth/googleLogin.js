ea1c83b90599c52b1780f3858f78c07ed8508e44
VasylBarna <vasylbarna@gmail.com>
2022-01-26 19:42:24 +0200

    чистка коментів

--- controllers/auth/googleLogin.js
+++ controllers/auth/googleLogin.js
@@ -94,51 +94,6 @@ const googleRedirect = async (req, res) => {
   );
 };
 
-// const googleRedirect = async (req, res) => {
-//   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-//   const urlObj = new URL(fullUrl);
-//   const urlParams = queryString.parse(urlObj.search);
-//   const code = urlParams.code;
-//   const tokenData = await axios({
-//     url: 'https://oauth2.googleapis.com/token',
-//     method: 'post',
-//     data: {
-//       client_id: GOOGLE_CLIENT_ID,
-//       client_secret: GOOGLE_CLIENT_SECRET,
-//       redirect_uri: `${BASE_URL}/api/auth/google-redirect`,
-//       grant_type: 'authorization_code',
-//       code,
-//     },
-//   });
-//   const userData = await axios({
-//     url: 'https://www.googleapis.com/oauth2/v2/userinfo',
-//     method: 'get',
-//     headers: {
-//       Authorization: `Bearer ${tokenData.data.access_token}`,
-//     },
-//   });
-//   const { email, name } = userData.data;
-//   let user = await User.findOne({ email });
-//   const avatar = gravatar.url(email);
-
-//   if (!user) {
-//     user = await User.create({
-//       ...req.body,
-//       name,
-//       email,
-//       avatar,
-//     });
-//   }
-
-//   const payload = { id: user._id };
-//   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-//   user = await User.findByIdAndUpdate(user._id, { token });
-
-//   return res.redirect(
-//     `${FRONTEND_URL}/google-redirect?token=${token}&email=${email}&name=${name}&avatar=${avatar}`,
-//   );
-// };
-
 module.exports = {
   googleLogin,
   googleRedirect,
